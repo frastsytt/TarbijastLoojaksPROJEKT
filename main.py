54258cf93c7833923fc6a8ef32e2432cc82316f1
TaaviU <46926501+staR42k@users.noreply.github.com>
2020-11-19 10:47:37 +0200

    main.py
    Kommentid main.py-s

---
+++ main.py
@@ -0,0 +1,15 @@
+# Lahenduse failis peab kommentaarina sisalduma ka püstitatud ülesande tekst ja autorite nimed.
+# 
+# HEAD COMMENTID.
+# 
+# 1 DEFINITION.
+# 
+# 1 IF LAUSE.
+# 
+# 1 WHILE / FOR LOOP.
+# 
+# READ FAILIST VÕI INPUT().
+# 
+# PRINT().
+# 
+# 1 ARRAY (LIST).
