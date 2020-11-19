9eabeb780d169a4fa4fca4195988b6e44e7adc63
TaaviU <46926501+staR42k@users.noreply.github.com>
2020-11-19 11:40:26 +0200

    Uus versioon

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 # Lahenduse failis peab kommentaarina sisalduma ka püstitatud ülesande tekst ja autorite nimed.
+#
+# Arvutab euro valuutast raha ümber uude valuutasse
 # 
 # HEAD COMMENTID.
 # 
-# 1 DEFINITION.
+# + 1 DEFINITION.
 # 
-# 1 IF LAUSE.
+# + 1 IF LAUSE.
 # 
-# 1 WHILE / FOR LOOP.
+# + 1 WHILE / FOR LOOP.
 # 
-# READ FAILIST VÕI INPUT().
+# + READ FAILIST VÕI INPUT().
 # 
-# PRINT().
+# + PRINT().
 # 
 # 1 ARRAY (LIST).
+
+
+def kurss():
+    pass
+
+
+def arvuta_kurss(raha, uus_valuuta):
+    if float(uus_valuuta) > 0:
+        uus_raha = float(raha) * float(uus_valuuta)
+        return uus_raha
+    else:
+        return "Sisestage positiivne kurss!"
+
+# Küsib kasutajalt vahetatava raha kogust ja ümber vahetamise kurssi.
+raha = input("Sisestage raha kogus: ")
+uus_valuuta = input("Sisestage valuuta kurss milleks raha vahetatakse: ")
+
+
+with open("main_valuuta_nimed.txt", "r") as valuuta_nimed:
+    for nimi in valuuta_nimed:
+        print(nimi)
+
+
+    #print(str(arvuta_kurss(raha, uus_valuuta)))
+    
+    
