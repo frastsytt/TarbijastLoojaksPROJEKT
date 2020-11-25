295785fe2caf12abfa7fe57822b97c4dced1a348
TaaviU <46926501+staR42k@users.noreply.github.com>
2020-11-25 10:31:42 +0200

    Final build
    Commentid puudu

--- internet.py
+++ internet.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 # Lahenduse failis peab kommentaarina sisalduma ka püstitatud ülesande tekst ja autorite nimed.
 #
@@ -16,35 +17,56 @@ import requests
 #
 # + PRINT().
 #
-# 1 ARRAY (LIST).
+# + 1 ARRAY (LIST).
 import urllib
 
 
-# def kurss():
-#     pass
-#
-#
-# def arvuta_kurss(raha, uus_valuuta):
-#     if float(uus_valuuta) > 0:
-#         uus_raha = float(raha) * float(uus_valuuta)
-#         return uus_raha
-#     else:
-#         return "Sisestage positiivne kurss!"
-#
-#
-# # Küsib kasutajalt vahetatava raha kogust ja ümber vahetamise kurssi.
-# raha = input("Sisestage raha kogus: ")
-# uus_valuuta = input("Sisestage valuuta kurss milleks raha vahetatakse: ")
-#
-# with open("main_valuuta_nimed.txt", "r") as valuuta_nimed:
-#     for nimi in valuuta_nimed:
-#         print(nimi)
-
-    # print(str(arvuta_kurss(raha, uus_valuuta)))
-
 source = requests.get(url="https://www.eestipank.ee/valuutakursid/export/csv/latest", )
+string = source.text
+postString = string.split("\n",3)[3]
 
-for kurss in source.text:
-    if len(open(source.text).readlines(  )):
-        pass
-print(source.text)
+
+andmefail = open("andmed.csv", "w", encoding="UTF-8")
+andmefail.write(postString)
+andmefail.close()
+
+valuuta_nimed = []
+kursi_väärtused = []
+
+
+with open('andmed.csv') as csvfile:
+    tabel = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in tabel:
+        valuuta_nimed.append(row[0])
+        kursi_väärtused.append(row[1])
+
+
+def valuutavahetus(nimi, raha):
+    if nimi.upper() in valuuta_nimed and type(raha) == float:
+        pärast_vahetust = round(raha * float(kursi_väärtused[valuuta_nimed.index(nimi.upper())]), 2)
+        return pärast_vahetust
+    else:
+        return "Sisestatud valuuta ei sobi."
+
+
+for line in valuuta_nimed:
+    print(line)
+
+while True:
+    try:
+        print("")
+        valuuta_nimi = str(input("Sisestage valuuta lühend: "))
+        print("")
+        raha_kogus = float(input("Sisestage vahetatava raha kogus: "))
+        print("")
+        print(valuutavahetus(valuuta_nimi, raha_kogus))
+        print("")
+    except ValueError:
+        print("")
+        print("Sisestage korrektsed sisendid. ")
+
+
+
+
+
+
